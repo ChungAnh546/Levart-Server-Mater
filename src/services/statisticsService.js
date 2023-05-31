d7b4b05f6bd8bf0d9543fa9d71cec5d2c881a556
EZ_Chung <anhchung546@gmail.com>
2023-06-01 02:33:57 +0700

    abc

--- src/services/statisticsService.js
+++ src/services/statisticsService.js
@@ -1,14 +1,17 @@
+
 import db from "../models/index";
 import moment from "moment";
+
 let totalBillOfDate = async (date) => {
     let dataBill = await db.Bill.findAll();
     let totalBill = 0;
     for (let index = 0; index < dataBill.length; index++) {
         const element = dataBill[index];
         const dateStatistics = moment(date, "DD/MM/YYYY");
-        const dateBill = moment(element.createdAt, "DD/MM/YYYY");
-        // console.log(date + '_' + dateBill);
+        const dateBill = moment(moment(element.createdAt).format('DD/MM/YYYY'), "DD/MM/YYYY");
+        //console.log(dateStatistics + '_' + dateBill);
         if (dateBill.isSame(dateStatistics)) {
+            // console.log(1);
             totalBill = totalBill + element.totalCost;
         }
     }
